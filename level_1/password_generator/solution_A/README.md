1f926cd69434173143cb50e39976abd3ce364e18
maryamhatami2005 <maryhatami2005@gmail.com>
2025-11-23 16:01:29 +0330

    update

---
+++ level_1/password_generator/solution_A/README.md
@@ -0,0 +1,29 @@
+# Password Generator(OOP approach)
+This project contains a password generator application written in Python. The generator can create three types of passwords:
+
+1. Random Passwords
+2. Memorable Passwords
+3. Pin Codes
+## Project Structure
+```
+├── README.md
+└── src
+    ├── main.py
+    └── test.ipynb
+```
+## Requirements
+ - Python 3.7+
+ - NLTK
+  
+to install NLTK, use pip:
+```
+pip install nltk
+```
+
+After installing NLTK, you need to download the 'words' corpus. Run Python and type these commands:
+
+``` import nltk
+nltk.download('words')
+```
+## Testing
+The `main.py` script also includes test cases for each password generator.
--- level_1/password_generator/solution_A/src/main.py
+++ level_1/password_generator/solution_A/src/main.py
@@ -1,29 +1,34 @@
 import random
 import string
 from abc import ABC, abstractmethod
+from typing import List, Optional
 
 import nltk
 
 nltk.download("words")
 
 class PasswordGenerator(ABC):
+    """ Base class for generating passwords.
+    """
     @abstractmethod
-    def generate(self):
+    def generate(self) -> str:
         pass
 
 class PinGenerator(PasswordGenerator):
+    """ Class to generate a numeric pin code.
+    """
     def __init__(self, length: int):
-        self.length = length
+        self.length: int = length
 
     def generate(self) -> str:
         return ''.join([random.choice(string.digits) for n in range(self.length)])
 
 
 class RandomPasswordGenerator(PasswordGenerator):
+    """ Class to generate a random password.
+    """
     def __init__(self, length: int = 8, include_numbers: bool = False, include_symbols: bool = False):
         self.length = length
-        self.include_numbers = include_numbers
-        self.include_symbols = include_symbols
         self.characters = string.ascii_letters
         if include_numbers:
             self.characters += string.digits
@@ -32,25 +37,27 @@ class RandomPasswordGenerator(PasswordGenerator):
 
 
     def generate(self) -> str:
-        return "".join([random.choice(self.characters) for n in range(self.length)])
+        return ''.join(random.choice(self.characters) for n in range(self.length))
     
 
 class MemorablePasswordGenerator(PasswordGenerator):
+    """ Class to generate a memorable password.
+    """
     def __init__(
         self, number_of_words: int = 4,
         seprator: str = '-',
         upper: bool = True,
-        vocabulary: list = None):
+        vocabulary:  Optional[List[str]] = None):
 
         if vocabulary is None:
             vocabulary = nltk.corpus.words.words()
 
-        self.number_of_words = number_of_words
-        self.upper = upper
-        self.seprator = seprator
-        self.vocabulary = vocabulary
+        self.number_of_words: int = number_of_words
+        self.upper: bool = upper
+        self.seprator: str = seprator
+        self.vocabulary: List[str] = vocabulary
 
-    def generate(self):
+    def generate(self) -> str:
         password_words = [random.choice(self.vocabulary) for n in range(self.number_of_words)]
         if self.upper:
             password_words = [word.upper() if random.choice([True, False]) else word.lower() for word in password_words]
@@ -58,8 +65,12 @@ class MemorablePasswordGenerator(PasswordGenerator):
     
 
 if __name__ == "__main__":
-    p_obj = RandomPasswordGenerator()
-    print(p_obj.generate())
 
     memorable_pass = MemorablePasswordGenerator()
     print(memorable_pass.generate())
+
+    pin = PinGenerator(4)
+    print(pin.generate())
+
+    random_pass = RandomPasswordGenerator()
+    print(random_pass.generate())

de2e3604907f9aca86a5cb1782ed1817af4f2fac
maryamhatami2005 <maryhatami2005@gmail.com>
2025-12-06 22:17:24 +0330

    update

---
+++ level_1/contact_book/src/main.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+
+
+class ContactBook:
+    def __init__(self):
+        self.contacts = defaultdict(dict)
+    
+    def add_contact(self, name, phone, email=None):
+        if name in self.contacts.keys() :
+            print("Contact Already Exist")
+            return
+        self.contacts[name]['phone'] = phone
+        self.contacts[name]['email'] = email
+
+    def view_contacts(self):
+        for name, info in self.contacts.items():
+            print(f"Name: {name}")
+            print(f"Phone: {info['phone']}")
+            print(f"Email: {info['email']}")
+            print("_" * 50)
+    
+    def delete_contact(self, name):
+        if name in self.contacts:
+            del self.contacts[name]
+            print("Contact deleted successfully")
+        else:
+            print("Contact not found")
+    
+    def update_contact(self, name, phone = None, email = None):
+        if name in self.contacts:
+            if phone:
+                self.contacts[name]['phone'] = phone
+            if email:
+                self.contacts[name]['email'] = email
+            print("Contact updated successfully")
+        else:
+            print("Contact not found")
+    
+
+if __name__ == "__main__":
+    book = ContactBook()
+    while True:
+        print("\nWelcom to contact book application")
+        print("1. Add Contact")
+        print("2. Edit Contact")
+        print("3. View Contacts")
+        print("4. Delete Contacts")
+        print("5. Quit")
+
+        user_choice = input("Please choose an option: ")
+
+        if user_choice == "5":
+            break
+        elif user_choice == "1":
+            name = input("Enter contact name: ")
+            phone = input("Enter contact phone: ")
+            email = input("Enter contact email: ")
+            book.add_contact(name.capitalize(), phone, email)
+
+        elif user_choice == "2":
+            name = input("Enter contact name: ")
+            phone = input("Enter contact phone: ")
+            email = input("Enter contact email: ")
+            book.update_contact(name.capitalize(), phone, email)
+
+        elif user_choice == "3":
+            print("List of contacts: ")
+            book.view_contacts()
+        
+        elif user_choice == "4":
+            name = input("Enter contact name: ")
+            book.delete_contact(name.capitalize())

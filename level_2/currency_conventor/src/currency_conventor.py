1700eebae5e3dba585b0dbede9eff9b6232f10dd
maryamhatami2005 <maryhatami2005@gmail.com>
2026-02-24 00:14:26 +0330

    updare

--- level_2/currency_conventor/src/currency_conventor.py
+++ level_2/currency_conventor/src/currency_conventor.py
@@ -1,6 +1,12 @@
 import requests
+from cachetools import cached, TTLCache
+import time
 
+cache = TTLCache(maxsize= 100, ttl= 3*60*60)
+
+@cached(cache)
 def get_exchange_rate(base_currency, target_currency):
+    time.sleep(2)
     url = f"https://api.exchangerate-api.com/v4/latest/{base_currency}"
     response = requests.get(url)
     if response.status_code != 200:

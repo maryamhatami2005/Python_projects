925e91590480ea3a252a8e7489c3211622983bdf
maryamhatami2005 <maryhatami2005@gmail.com>
2026-07-26 18:52:12 +0330

    update

--- level_2/currency_conventor/src/currency_conventor.py
+++ level_2/currency_conventor/src/currency_conventor.py
@@ -7,7 +7,7 @@ cache = TTLCache(maxsize= 100, ttl= 3*60*60)
 @cached(cache)
 def get_exchange_rate(base_currency, target_currency):
     time.sleep(2)
-    url = f"https://api.exchangerate-api.com/v4/latest/{base_currency}"
+    url = f"https://open.er-api.com/v6/latest/{base_currency}"
     response = requests.get(url)
     if response.status_code != 200:
         return None

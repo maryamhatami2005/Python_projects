deb73046b0950da555fb7beadc4031fef1aa9cbc
maryamhatami2005 <maryhatami2005@gmail.com>
2026-07-29 23:14:40 +0330

    update

---
+++ level_2/youtube_downloader/argparse/test_argparser.py
@@ -0,0 +1,12 @@
+import sys
+
+def main(url, quality, output_path):
+    print(url, quality, output_path)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python test_argparser.py <url> <quality> <output_path")
+    print(sys.argv)
+    url, quality, output_path = sys.argv[1:]
+
+    main(url, quality, output_path)
